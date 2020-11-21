9e56e4ca1e847b9b7f14557e232e291718d0815f
XiEight <electricsix23@gmail.com>
2020-11-21 16:51:30 +0300

    Registation prototype writed

---
+++ src/Server/build/CMakeFiles/FileSharingSystemServer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/File-Sharing-System/src/Server/API/RequestHandler.cpp" "/home/alex/File-Sharing-System/src/Server/build/CMakeFiles/FileSharingSystemServer.dir/API/RequestHandler.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/API/Server.cpp" "/home/alex/File-Sharing-System/src/Server/build/CMakeFiles/FileSharingSystemServer.dir/API/Server.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/BL/AccountManager.cpp" "/home/alex/File-Sharing-System/src/Server/build/CMakeFiles/FileSharingSystemServer.dir/BL/AccountManager.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Common/EMailManager.cpp" "/home/alex/File-Sharing-System/src/Server/build/CMakeFiles/FileSharingSystemServer.dir/Common/EMailManager.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Common/databaseobject.cpp" "/home/alex/File-Sharing-System/src/Server/build/CMakeFiles/FileSharingSystemServer.dir/Common/databaseobject.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/main.cpp" "/home/alex/File-Sharing-System/src/Server/build/CMakeFiles/FileSharingSystemServer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Server/build/CMakeFiles/FileSharingSystemServer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileSharingSystemServer.dir/API/RequestHandler.cpp.o"
+  "CMakeFiles/FileSharingSystemServer.dir/API/Server.cpp.o"
+  "CMakeFiles/FileSharingSystemServer.dir/BL/AccountManager.cpp.o"
+  "CMakeFiles/FileSharingSystemServer.dir/Common/EMailManager.cpp.o"
+  "CMakeFiles/FileSharingSystemServer.dir/Common/databaseobject.cpp.o"
+  "CMakeFiles/FileSharingSystemServer.dir/main.cpp.o"
+  "FileSharingSystemServer"
+  "FileSharingSystemServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FileSharingSystemServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
