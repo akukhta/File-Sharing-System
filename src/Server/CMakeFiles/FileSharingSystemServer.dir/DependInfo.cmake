6a1a2c43a759d6b5dbbc241a62251f3d04ca90ff
XiEight <electricsix23@gmail.com>
2021-01-21 19:06:12 +0300

    !

---
+++ src/Server/CMakeFiles/FileSharingSystemServer.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alex/File-Sharing-System/src/Server/API/RequestHandler.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/API/RequestHandler.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/API/Server.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/API/Server.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/BL/AccountManager.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/BL/AccountManager.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/BL/NodesManager.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/BL/NodesManager.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Common/Configuration.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Common/Configuration.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Common/Node.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Common/Node.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Data/AccountsRepository.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Data/AccountsRepository.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Data/DataBaseSingleton.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Data/DataBaseSingleton.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Data/EMailManager.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Data/EMailManager.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Data/NodesRepository.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Data/NodesRepository.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/Data/databaseobject.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/Data/databaseobject.cpp.o"
+  "/home/alex/File-Sharing-System/src/Server/main.cpp" "/home/alex/File-Sharing-System/src/Server/CMakeFiles/FileSharingSystemServer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/Server/main.cpp
+++ src/Server/main.cpp
@@ -8,23 +8,11 @@ using namespace std;
 
 int main()
 {
-    std::shared_ptr<DataBaseObject> db = std::make_shared<DataBaseObject>();
-    std::unique_ptr<AbstractAccountRepository> accRep = std::make_unique<AccountsRepository>(db);
-    std::unique_ptr<AbstractNodesRepository> nodeRep = std::make_unique<NodesRepository>(db);
-    std::unique_ptr<AccountManager> accountManager = std::make_unique<AccountManager>(accRep);
-    std::unique_ptr<NodesManager> nodesManager = std::make_unique<NodesManager>(nodeRep);
-    std::unique_ptr<RequestHandler> handler = std::make_unique<RequestHandler>(accountManager, nodesManager);
-    std::unique_ptr<Server> server = std::make_unique<Server>(handler);
 
-    try{
-        server->run();
-    }
-     catch (std::runtime_error const & err)
-    {
-        std::cout << err.what() << std::endl;
-    }
-//    This code doesn`t work, but if i replace all smart pointers to references, it'll be ok
-//    auto injector = boost::di::make_injector(boost::di::bind<shared_ptr<DataBaseObject>>.to());
-//    injector.create<Server>().run();
+    //This code doesn`t work, but if i replace all smart pointers to references, it'll be ok
+    auto injector = boost::di::make_injector(
+                boost::di::bind<AbstractAccountRepository>.to<AccountsRepository>(),
+                boost::di::bind<AbstractNodesRepository>.to<NodesRepository>());
+    injector.create<Server>().run();
     return 0;
 }
