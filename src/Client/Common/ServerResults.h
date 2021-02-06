0eeaad192be0e7621f21d1a31676d1faa8b92b0f
XiEight <electricsix23@gmail.com>
2021-02-06 11:51:21 +0300

    2

--- src/Client/Common/ServerResults.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-/*
-    This class shows a result of user's operation on the server's side.
-    Failure - a called function has failed or throwed an exception
-    Success - a called function has finished correctly
-*/
-enum class ServerResult : bool {Failure = false, Success = true};
--- src/Client/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QApplication>
-#include "Application.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Application app;
-    app.start();
-}
